0692d93fa02feb3d5e7b81c0a924ca74e45cd90e
engineervinay <vinay1645.vp@gmail.com>
2022-08-28 04:36:32 -0700

    netlify error resolve

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 export default function Home() {
   
   
