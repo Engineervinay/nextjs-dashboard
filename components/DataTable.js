c2e74ebd01a8fbca94befebe29cc89991d4135d9
engineervinay <vinay1645.vp@gmail.com>
2022-08-28 04:28:03 -0700

    netlify resolve

--- components/DataTable.js
+++ components/DataTable.js
@@ -3,53 +3,53 @@ import React from 'react'
 export default function DataTable() {
   return (
     
-<div class="overflow-x-auto relative">
-    <table class="w-full text-sm text-left text dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+<div className="overflow-x-auto relative">
+    <table className="w-full text-sm text-left text dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                    ID Number
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                     Nickname
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                     Registration Time
                 </th>
                
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     1
                 </th>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                     2
                 </td>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                     3
                 </td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                    4
                 </th>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                    5
                 </td>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                     6
                 </td>
             </tr>
-            <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <tr className="bg-white dark:bg-gray-800">
+                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     7
                 </th>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                     8
                 </td>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                    9
                 </td>
             </tr>
--- components/Layout.js
+++ components/Layout.js
@@ -13,7 +13,7 @@ export default function Layout () {
     <div className="rounded-lg bg-fgcolor dark:bg-darkfg  m-4 flex-1 p-4 text-text">
     <div className="flex flex-row justify-end  gap-5">
     <div className="relative h-5 text-gray-600">
-        <input type="search" name="serch" placeholder="Search" class="bg-searchbar h-5 px-6 pr-10 rounded-full text-sm focus:outline-none"/>
+        <input type="search" name="serch" placeholder="Search" className="bg-searchbar h-5 px-6 pr-10 rounded-full text-sm focus:outline-none"/>
     </div>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
   <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
