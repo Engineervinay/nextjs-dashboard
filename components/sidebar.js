db589e106e6d8bfd6a35fcca430a2d3ea73f7a97
engineervinay <vinay1645.vp@gmail.com>
2022-08-29 05:49:15 -0700

    logout menu

--- components/sidebar.js
+++ components/sidebar.js
@@ -6,12 +6,15 @@ export default function Sidebar(){
     const {theme, setTheme} = useTheme()
     return(
         <div className='h-screen py-4 px-4 pb-8 mr-0 pr-0 bg-bgcolor dark:bg-darkbg flex justify-start flex-col align-start w-56 gap-5'>
-            <div className='flex flex-row justify-end'>
+            <div className='flex flex-row justify-end '>
                 <button
                 aria-label="Toggle "
                 type="button"
-                className="p-3 h-12 w-12 bg-white"
-                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>Dark Mode</button>
+                className="self-center p-1 justify-self-center m-2 h-10 w-10 bg-bgcolor rounded-lg dark:bg-darkbg border"
+                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+              </svg>
+              </button>
             
             </div> 
             <div>
@@ -19,19 +22,19 @@ export default function Sidebar(){
 
             </div>
             <div className=' py-4 pr-0 px-3 '>
-                <ul class="space-x-2">
+                <ul class="space-x-2 space-y-2 border-t border-gray-200 dark:border-gray-700">
                     
 
                 {DASHBOARD_DATA.map((it) => (
-            <li>
+            <li className='ml-0'>
  {it.status==false ? (
-    <a href="#" className="flex items-center p-2 pr-0 text-base font-normal text-gray-900 rounded-full rounded-r-none dark:text-white hover:bg-fgcolor dark:hover:bg-darkfg">
+    <a href="#" className="flex items-center p-2 pr-0 py-1 text-base font-normal text-gray-900 rounded-full rounded-r-none dark:text-white hover:bg-fgcolor hover:text-bgcolor dark:hover:bg-darkfg">
         <div className='w-5'> {it.icon}</div>
         <span className="ml-3">{it.title}</span>
     </a>
 
     ):(
-        <a href="#" className="flex items-center p-2 pr-0 bg-fgcolor text-base font-normal text-blue-500 rounded-full rounded-r-none dark:text-white hover:bg-fgcolor dark:bg-darkfg dark:hover:bg-darkfg">
+        <a href="#" className="flex items-center p-2 pr-0 py-1 bg-fgcolor text-base font-normal text-bgcolor rounded-full rounded-r-none dark:text-white hover:bg-fgcolor dark:bg-darkfg dark:hover:bg-darkfg">
             <div className='w-5'> {it.icon}</div>
             <span className="ml-3">{it.title}</span>
         </a>
@@ -45,6 +48,18 @@ export default function Sidebar(){
 
 
                     
+                </ul>
+
+                <ul className='my-2 border-t border-gray-200 dark:border-gray-700 '>
+                <li className='my-2 ml-0'>
+                <a href="#" className="flex items-center p-2 pr-0 py-1  text-base font-normal  rounded-full rounded-r-none dark:text-white hover:bg-fgcolor hover:text-bgcolor dark:bg-darkfg dark:hover:bg-darkfg">
+            <div className='w-5'> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+</svg>
+</div>
+            <span className="ml-3">Logout</span>
+        </a>
+                </li>
                 </ul>   
             </div>
                         
